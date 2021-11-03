c8a6c7d95866f2258ffc440d36a4b528bb2fd509
vladimir <qw3rty.asdd@yandex.ru>
2021-11-03 22:26:02 +0400

    Добавление контроллера для API

---
+++ views/api/fullapi.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\bootstrap4\ActiveForm;
+use yii\bootstrap4\Html;
+use yii\captcha\Captcha;
+?>
+
+<div class="row">
+    <div class="col-lg-5">
+
+        <div class="container">
+            <form id="form" method="post">
+                <input type="text" placeholder="FromLat" name="FromLat">
+                <input type="text" placeholder="FromLng" name="FromLng">
+                <input type="text" placeholder="ToLat" name="ToLat">
+                <input type="text" placeholder="ToLng" name="ToLng">
+                <input type="submit" value="Выполнить" name="submit">
+            </form>
+        </div>
+
+    </div>
+    <div class="container">
+        <?php
+        echo '<pre>';
+            $turns!=null? print_r($turns) : print("Координаты не заданы");
+        echo '</pre>';
+        ?>
+    </div>
+</div>
