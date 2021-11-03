bc4c3323617ac3b3d55b2ac78df8738559696da3
vladimir <qw3rty.asdd@yandex.ru>
2021-11-03 22:25:13 +0400

    Добавление контроллера для API

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -6,5 +6,49 @@ use yii\web\Controller;
 
 class ApiController extends Controller
 {
+    public $enableCsrfValidation = false;
+    public function actionGetapi()  
+    {
+        $content = array();
+        if (isset($_POST['submit'])) $content = $this->getapi();
+        return $this->render('getapi',['turns' => $content]);
+    }
+    
+    public function actionFullapi()
+    {
+        $content = array();
+        if (isset($_POST['submit'])) $content = $this->getfullapi();
+        return $this->render('getapi',['turns' => $content]);
+    }
 
+    
+    private function getapi()
+    {   
+            $curl = "http://open.mapquestapi.com/directions/v2/route?key=dtKoIJL6pOrvQKs8vKJcmDx0IhGmonjF&from=" . $_POST['FromLat'] . "," . $_POST['FromLng'] . "&to=" . $_POST['ToLat'] . "," . $_POST['ToLng'];
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_URL, $curl);
+
+            $responce = curl_exec($ch);
+            curl_close($ch);
+            $data = json_decode($responce, true);
+            $turns = array();
+            if ($data['route']['legs']==null ) return "Маршрут не найден";
+                foreach ($data['route']['legs']['0']['maneuvers'] as $startpoint) 
+                    array_push($turns, $startpoint['startPoint']);
+            return $turns;
+            
+    }
+    private function getfullapi()
+    {   
+            $curl = "http://open.mapquestapi.com/directions/v2/route?key=dtKoIJL6pOrvQKs8vKJcmDx0IhGmonjF&from=" . $_POST['FromLat'] . "," . $_POST['FromLng'] . "&to=" . $_POST['ToLat'] . "," . $_POST['ToLng'];
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_URL, $curl);
+
+            $responce = curl_exec($ch);
+            curl_close($ch);
+            $data = json_decode($responce, true);
+            return $data;        
+    }
 }
--- views/site/api.php
+++ views/site/api.php
@@ -9,39 +9,32 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-api">
     <div class="form_container">
-                <form id="form" method="post">
-                    <input type="text" placeholder="FromLat" name="FromLat" >
-                    <input type="text" placeholder="FromLng" name="FromLng" >
-                    <input type="text" placeholder="ToLat" name="ToLat" >
-                    <input type="text" placeholder="ToLng" name="ToLng" >
-                    <input type="submit" value="Выполнить" >
-                </form>
+        <form id="form" method="post">
+            <input type="text" placeholder="FromLat" name="FromLat">
+            <input type="text" placeholder="FromLng" name="FromLng">
+            <input type="text" placeholder="ToLat" name="ToLat">
+            <input type="text" placeholder="ToLng" name="ToLng">
+            <input type="submit" value="Выполнить">
+        </form>
     </div>
 
 
 
     <div class="api-content">
         <?php
-            if ($_POST['FromLat']!='' and $_POST['FromLng']!='' and $_POST['ToLat']!='' and $_POST['ToLat']!='')
-            {            
-                $curl = "http://open.mapquestapi.com/directions/v2/route?key=dtKoIJL6pOrvQKs8vKJcmDx0IhGmonjF&from=".$_POST['FromLat'].",".$_POST['FromLng']."&to=".$_POST['ToLat'].",".$_POST['ToLng'];
-                $ch=curl_init();
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_URL, $curl);
+        if ($_POST['FromLat'] != '' and $_POST['FromLng'] != '' and $_POST['ToLat'] != '' and $_POST['ToLat'] != '') {
+            $curl = "http://open.mapquestapi.com/directions/v2/route?key=dtKoIJL6pOrvQKs8vKJcmDx0IhGmonjF&from=" . $_POST['FromLat'] . "," . $_POST['FromLng'] . "&to=" . $_POST['ToLat'] . "," . $_POST['ToLng'];
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_URL, $curl);
 
-                $responce=curl_exec($ch);
-                curl_close($ch);
-                $data=json_decode($responce,true);
-                echo '<pre>';
-                $i = 1;
-                foreach($data['route']['legs']['0']['maneuvers'] as $startpoint)
-                {
-                    echo('<br>'.'ПОВОРОТ '.$i.'<br>');
-                    foreach($startpoint['startPoint'] as $key => $value) echo "<b>".$key."</b>".':'.$value.'<br>';
-                    $i++;
-                }
-            }
+            $responce = curl_exec($ch);
+            curl_close($ch);
+            $data = json_decode($responce, true);
+            echo '<pre>';
+            $i = 1;
+            print_r($data);
+        }
         ?>
     </div>
 </div>
-
