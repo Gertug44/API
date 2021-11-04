20034d23a582778dbcc7f2e477f6a9c4444d89dd
vladimir <qw3rty.asdd@yandex.ru>
2021-11-04 04:56:56 +0400

    Теперь угол поворота дописывается также в исходный массив

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace app\controllers;
 
+use Exception;
 use yii\web\Controller;
 
 class ApiController extends Controller
@@ -25,42 +26,37 @@ class ApiController extends Controller
 
     private function getturns()
     {
-        $curl = "http://open.mapquestapi.com/directions/v2/route?key=dtKoIJL6pOrvQKs8vKJcmDx0IhGmonjF&from=" . $_POST['FromLat'] . "," . $_POST['FromLng'] . "&to=" . $_POST['ToLat'] . "," . $_POST['ToLng'];
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_URL, $curl);
-
-        $responce = curl_exec($ch);
-        curl_close($ch);
-        $data = json_decode($responce, true);
+        $data = $this->getfullapi();
         $turns = array();
-        if ($data['route']['legs'] == null) return "Маршрут не найден";
-        foreach ($data['route']['legs']['0']['maneuvers'] as $startpoint)
-            $turns[] = $startpoint['startPoint'];
-            $turns = $this->InsertIntoArr($turns);
-        return $turns;
+        try 
+        {
+            foreach ($data['route']['legs']['0']['maneuvers'] as &$startpoint) 
+            $turns[] = &$startpoint['startPoint'];
+            $this->InsertIntoArr($turns);
+            return $turns;
+        } 
+        catch (Exception $e) 
+        {
+            return "Маршрут не найден,  " . $e->getMessage();
+        }
     }
 
-    private function InsertIntoArr($array)
+    private function InsertIntoArr(&$array)
     {
-        for($i=1;$i<=count($array)-2;$i++)
-        {
-            $prev = $array[$i-1];
-            $current = $array[$i];
-            $next = $array[$i+1];
-            $array[$i]['angle'] = $this->angle($prev,$current,$next);
+        for ($i = 1; $i <= count($array) - 2; $i++) {
+            $array[$i]['angle'] = $this->angle($array[$i - 1], $array[$i], $array[$i + 1]);
         }
-         return $array;
+        return $array;
     }
 
     public function angle($prev, $current, $next)
     {
-        $vectorX = array($current['lat']-$prev['lat'] , $current['lng']-$prev['lng']);
-        $vectorY = array($next['lat']-$current['lat'] , $next['lng']-$current['lng']);
+        $vectorX = array($current['lat'] - $prev['lat'], $current['lng'] - $prev['lng']);
+        $vectorY = array($next['lat'] - $current['lat'], $next['lng'] - $current['lng']);
         $scalarmul = $vectorX[0] * $vectorY[0] + $vectorX[1] * $vectorY[1];
-        $LengthX = sqrt(pow($vectorX[0],2) + pow($vectorX[1],2));
-        $LengthY = sqrt(pow($vectorY[0],2) + pow($vectorY[1],2)); 
-        return round((($scalarmul / ($LengthX*$LengthY))),4);
+        $LengthX = sqrt(pow($vectorX[0], 2) + pow($vectorX[1], 2));
+        $LengthY = sqrt(pow($vectorY[0], 2) + pow($vectorY[1], 2));
+        return round((($scalarmul / ($LengthX * $LengthY))), 4);
     }
 
 
@@ -70,7 +66,6 @@ class ApiController extends Controller
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_URL, $curl);
-
         $responce = curl_exec($ch);
         curl_close($ch);
         $data = json_decode($responce, true);
